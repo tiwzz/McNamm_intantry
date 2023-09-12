06ee14071fc77029fe25bf539db19159494a6dec
tiwzz <1615726721@qq.com>
2023-09-12 22:34:53 +0800

    老麦基本完善，仅pitch轴很抖

---
+++ STANDARD1.7/application/vision_task.c
@@ -0,0 +1,341 @@
+/**
+ * @file vision_task.c
+ * @author yuanluochen
+ * @brief 发送自身yaw轴pitch轴roll轴绝对角度给上位机视觉，并解算视觉接收数据，由于hal库自身原因对全双工串口通信支持不是特别好，
+ *        为处理这个问题将串口数据分析，与串口发送分离，并延长串口发送时间
+ * @version 0.1
+ * @date 2023-03-11
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "vision_task.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "shoot_task.h"
+#include "gimbal_behaviour.h"
+//视觉发送任务结构体初始化
+static void vision_send_task_init(vision_send_t* init);
+//视觉任务初始化
+static void vision_task_init(vision_control_t* init);
+//视觉发送任务数据更新
+static void vision_send_task_feedback_update(vision_send_t* update);
+//视觉人物数据更新
+static void vision_task_feedback_update(vision_control_t* update);
+//数据编码
+static void vision_tx_encode(uint8_t* buf, float yaw, float pitch, float roll, uint8_t mode_switch);
+//设置yaw轴pitch轴增量
+static void vision_analysis_date(vision_control_t* vision_set);
+//发送数据
+static void send_message_to_vision(UART_HandleTypeDef* send_message_usart, DMA_HandleTypeDef* send_message_dma, uint8_t* send_message, uint16_t send_message_size);
+
+/**
+ * @brief 分析视觉原始增加数据，根据原始数据，判断是否要进行发射
+ * 
+ * @param shoot_judge 视觉结构体
+ * @param vision_begin_add_yaw_angle 上位机视觉yuw轴原始增加角度
+ * @param vision_begin_add_pitch_angle 上位机视觉pitch轴原始增加角度
+ */
+static void vision_shoot_judge(vision_control_t* shoot_judge, fp32 vision_begin_add_yaw_angle, fp32 vision_begin_add_pitch_angle);
+
+/**
+ * @brief 上位机数据发送
+ * 
+ * @param vision_send 上位机数据发送结构体
+ */
+static void vision_send_msg(vision_send_t* vision_send);
+
+
+
+
+//视觉任务结构体
+vision_control_t vision_control = { 0 };
+//视觉发送任务结构体
+vision_send_t vision_send = { 0 };
+
+//未接收到视觉数据标志位，该位为1 则未接收
+bool_t not_rx_vision_data_flag = 1;
+
+    //视觉发送任务结构体初始化
+        //发送当前位姿传递给上位机视觉
+        vision_send_msg(&vision_send);
+        //系统延时
+
+void vision_task_init(vision_control_t* init)
+{
+    // 获取陀螺仪绝对角指针                                                                                                                                                                                                                                                                                                                                                           init->vision_angle_point = get_INS_angle_point();
+    init->vision_angle_point = get_INS_angle_point();
+	
+    // 获取上位机视觉指针
+    init->vision_rxfifo = get_vision_rxfifo_point();
+    //初始化发射模式为停止袭击
+    init->shoot_vision_control.shoot_command = SHOOT_STOP_ATTACK;
+
+    vision_task_feedback_update(init);
+ 
+}
+void vision_task(void const* pvParameters)
+{
+    // 延时等待，等待上位机发送数据成功
+    vTaskDelay(VISION_TASK_INIT_TIME);
+    // 视觉任务初始化
+    vision_task_init(&vision_control);
+    while (1)
+    {
+        // 等待预装弹完毕，以及云台底盘初始化完毕，判断任务是否进行
+//        if (shoot_control_vision_task() && gimbal_control_vision_task())
+//        {
+            // 更新数据
+            vision_task_feedback_update(&vision_control);
+            // 解析上位机数据,配置yaw轴pitch轴增量,以及判断是否发射
+            vision_analysis_date(&vision_control);
+//        }
+//        else
+//        {
+//            //任务不运行
+//        }
+        // 系统延时
+        vTaskDelay(VISION_CONTROL_TIME_MS);
+    }
+}
+
+static void vision_send_msg(vision_send_t* vision_send)
+{
+    //数据更新
+    vision_send_task_feedback_update(vision_send);
+
+    //配置串口发送数据,编码
+    vision_tx_encode(vision_send->send_message, vision_send->send_absolution_angle.yaw * RADIAN_TO_ANGLE,
+                                                vision_send->send_absolution_angle.pitch * RADIAN_TO_ANGLE,
+                                                vision_send->send_absolution_angle.roll * RADIAN_TO_ANGLE,
+                                                ARMOURED_PLATE);
+    //串口发送
+    send_message_to_vision(vision_send->send_message_usart, vision_send->send_message_dma, vision_send->send_message, SERIAL_SEND_MESSAGE_SIZE);
+    
+}
+
+static void send_message_to_vision(UART_HandleTypeDef* send_message_usart, DMA_HandleTypeDef* send_message_dma, 
+                                  uint8_t* send_message, uint16_t send_message_size)
+{
+    //等待上一次数据发送
+    while (HAL_DMA_GetState(send_message_dma) == HAL_DMA_STATE_BUSY)
+    {
+        static int count = 0;
+        if (count ++ >= 1000)
+        {
+            break;
+        }
+    }
+    //关闭DMA
+    __HAL_DMA_DISABLE(send_message_dma);
+
+    //发送数据
+    HAL_UART_Transmit_DMA(send_message_usart, send_message, send_message_size);
+}
+
+static void vision_send_task_init(vision_send_t* init)
+{
+    //获取陀螺仪绝对角指针                                                                                                                                                                                                                                                                                                                                                           init->vision_angle_point = get_INS_angle_point();
+    init->vision_angle_point = get_INS_angle_point();
+    //初始化发送串口名称
+    init->send_message_usart = &VISION_USART;
+    //初始化发送dma
+    init->send_message_dma = &VISION_TX_DMA;
+
+    // 数据更新
+    vision_send_task_feedback_update(init);
+}
+
+static void vision_send_task_feedback_update(vision_send_t* update)
+{
+    // 获取原始数据
+    update->absolution_angle.yaw = *(update->vision_angle_point + INS_YAW_ADDRESS_OFFSET);
+    update->absolution_angle.pitch = -*(update->vision_angle_point + INS_PITCH_ADDRESS_OFFSET);
+    update->absolution_angle.roll = *(update->vision_angle_point + INS_ROLL_ADDRESS_OFFSET);
+
+    //更新发送数据,为处理负号，数据加180
+    update->send_absolution_angle.yaw = update->absolution_angle.yaw + SEND_MESSAGE_ERROR;
+    update->send_absolution_angle.pitch = update->absolution_angle.pitch + SEND_MESSAGE_ERROR;
+    update->send_absolution_angle.roll = update->absolution_angle.roll + SEND_MESSAGE_ERROR;
+}
+static void vision_task_feedback_update(vision_control_t* update)
+{
+    // 获取原始数据并转化为角度制
+    update->absolution_angle.yaw = *(update->vision_angle_point + INS_YAW_ADDRESS_OFFSET) * RADIAN_TO_ANGLE;
+    update->absolution_angle.pitch = -*(update->vision_angle_point + INS_PITCH_ADDRESS_OFFSET) * RADIAN_TO_ANGLE;
+    update->absolution_angle.roll = *(update->vision_angle_point + INS_ROLL_ADDRESS_OFFSET) * RADIAN_TO_ANGLE;
+
+}
+static void vision_tx_encode(uint8_t* buf, float yaw, float pitch, float roll, uint8_t mode_switch)
+{
+    //数据起始
+    date32_to_date8_t head1_temp = { 0 };
+    date32_to_date8_t head2_temp = { 0 };
+    //yaw轴数值转化
+    date32_to_date8_t yaw_temp = { 0 };
+    //pitch轴数值转化
+    date32_to_date8_t pitch_temp = { 0 };
+    //roll轴数据转化
+    date32_to_date8_t roll_temp = { 0 };
+    //模式转换
+    date32_to_date8_t switch_temp = { 0 };
+    //中止位
+    date32_to_date8_t end_temp = { 0 };
+
+    //赋值数据
+    head1_temp.uint32_val = HEAD1_DATA;
+    head2_temp.uint32_val = HEAD2_DATA;
+    yaw_temp.uint32_val = yaw * DOUBLE_;
+    pitch_temp.uint32_val = pitch * DOUBLE_;
+    roll_temp.uint32_val = roll * DOUBLE_;  
+    switch_temp.uint32_val = mode_switch;
+    end_temp.uint32_val = 0x0A;//"/n"
+
+    for (int i = 3; i >= 0; i--)
+    {
+        int j = -(i - 3);//数据位置转换
+        buf[HEAD1_ADDRESS_OFFSET * UINT8_T_DATA_SIZE + j] = head1_temp.uin8_value[i];
+        buf[HEAD2_ADDRESS_OFFSET * UINT8_T_DATA_SIZE + j] = head2_temp.uin8_value[i];
+        buf[YAW_ADDRESS_OFFSET * UINT8_T_DATA_SIZE + j] = yaw_temp.uin8_value[i];
+        buf[PITCH_ADDRESS_OFFSET * UINT8_T_DATA_SIZE + j] = pitch_temp.uin8_value[i];
+        buf[ROLL_ADDRESS_OFFSET * UINT8_T_DATA_SIZE + j] = roll_temp.uin8_value[i];
+        buf[SWITCH_ADDRESS_OFFSET * UINT8_T_DATA_SIZE + j] = switch_temp.uin8_value[i];
+        buf[END_ADDRESS_OFFSET * UINT8_T_DATA_SIZE + j] = end_temp.uin8_value[i];
+    }
+}
+
+static void vision_analysis_date(vision_control_t *vision_set)
+{
+
+    // 上位机视觉版本
+    static fp32 vision_gimbal_yaw = 0;   // yaw轴绝对角
+    static fp32 vision_gimbal_pitch = 0; // pitch轴绝对角
+    // 未接收到上位机的时间
+    static int32_t unrx_time = MAX_UNRX_TIME;
+
+    // 判断当前云台模式为自瞄模式
+        // 是自瞄模式，设置角度为上位机设置角度
+
+        // 判断是否接收到上位机数据
+        if (vision_set->vision_rxfifo->rx_flag) // 识别到目标
+        {
+            // 接收到数据标志位为0
+            not_rx_vision_data_flag = 0;
+
+            unrx_time = 0;
+            // 接收到上位机数据
+            // 接收标志位 置零
+            vision_set->vision_rxfifo->rx_flag = 0;
+
+            // 获取上位机视觉数据
+            vision_gimbal_pitch = vision_set->vision_rxfifo->pitch_fifo;
+            vision_gimbal_yaw = vision_set->vision_rxfifo->yaw_fifo;
+        }
+
+        // 判断上位机视觉停止发送指令
+        if (unrx_time >= MAX_UNRX_TIME)
+        {
+            // 数据置零
+            unrx_time = 0;
+            // 停止发弹
+            vision_set->shoot_vision_control.shoot_command = SHOOT_STOP_ATTACK;
+            not_rx_vision_data_flag = 1;
+        }
+    }
+
+    // 赋值控制值
+    // 判断是否控制值被赋值
+    if (vision_gimbal_pitch == 0 && vision_gimbal_yaw == 0)
+    {
+
+        // 未赋值依旧为当前值
+        vision_set->gimbal_vision_control.gimbal_pitch = vision_set->absolution_angle.pitch;
+        vision_set->gimbal_vision_control.gimbal_yaw = vision_set->absolution_angle.yaw;
+    }
+    else
+    {
+        // 已赋值，用设置值
+        vision_set->gimbal_vision_control.gimbal_pitch = vision_gimbal_pitch;
+        vision_set->gimbal_vision_control.gimbal_yaw = vision_gimbal_yaw;
+    }
+}
+
+
+/**
+ * @brief 分析视觉原始增加数据，根据原始数据，判断是否要进行发射，判断yaw轴pitch的角度，如果在一定范围内，则计算值增加，增加到一定数值则判断发射，如果yaw轴pitch轴角度大于该范围，则计数归零
+ * 
+ * @param shoot_judge 视觉结构体
+ * @param vision_begin_add_yaw_angle 上位机视觉yuw轴原始增加角度
+ * @param vision_begin_add_pitch_angle 上位机视觉pitch轴原始增加角度
+ */
+static void vision_shoot_judge(vision_control_t* shoot_judge, fp32 vision_begin_add_yaw_angle, fp32 vision_begin_add_pitch_angle)
+{
+    // 判断击打计数
+    static int attack_count = 0;
+    // 判断停止击打的次数 
+    static int stop_attack_count = 0;
+
+    
+    // 上位机发送角度到一定方位内计数值增加
+    if (fabs(vision_begin_add_pitch_angle) <= ALLOW_ATTACK_ERROR && fabs(vision_begin_add_yaw_angle) <= ALLOW_ATTACK_ERROR)
+    {
+        // 停止击打计数值归零
+        stop_attack_count = 0;
+
+        // 判断计数值是否到达判断击打的计数值
+        if (attack_count >= JUDGE_ATTACK_COUNT)
+        {
+            // 到达可击打的次数
+            // 设置击打
+            shoot_judge->shoot_vision_control.shoot_command = SHOOT_ATTACK;
+        }
+        else
+        {
+            // 未到达可击打的次数
+            // 计数值增加
+            attack_count++;
+        }
+    }
+    // 上位机发射角度大于该范围计数值归零
+    else if (fabs(vision_begin_add_pitch_angle) > ALLOW_ATTACK_ERROR || fabs(vision_begin_add_yaw_angle) > ALLOW_ATTACK_ERROR)
+    {
+        
+
+        if (stop_attack_count >= JUDGE_STOP_ATTACK_COUNT)
+        {
+            //达到停止击打的计数
+            // 判断击打计数值归零
+            attack_count = 0;
+            //设置停止击打
+            shoot_judge->shoot_vision_control.shoot_command = SHOOT_STOP_ATTACK;
+        }
+        else
+        {
+            // 未到达停止击打的次数
+            // 计数值增加
+            stop_attack_count ++;
+        }
+
+        
+    }
+}
+
+
+bool_t judge_not_rx_vision_data(void)
+{
+    return not_rx_vision_data_flag;
+}
+
+// 获取上位机云台命令
+gimbal_vision_control_t* get_vision_gimbal_point(void)
+{
+    return &vision_control.gimbal_vision_control;
+}
+
+// 获取上位机发射命令
+shoot_vision_control_t* get_vision_shoot_point(void)
+{
+    return &vision_control.shoot_vision_control;
+}
+ 
---
+++ STANDARD1.7/application/vision_task.h
@@ -0,0 +1,255 @@
+/**
+ * @file vision_task.h
+ * @author yuanluochen
+ * @brief 发送自身yaw轴pitch轴roll轴绝对角度给上位机视觉，并解算视觉接收数据，由于hal库自身原因对全双工串口通信支持不是特别好，
+ *        为处理这个问题将串口数据分析，与串口发送分离，并延长串口发送时间
+ * @version 0.1
+ * @date 2023-03-11
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#ifndef VISION_TASK_H
+#define VISION_TASK_H
+
+#include "usart.h"
+#include "dma.h"
+#include "INS_task.h"
+#include "arm_math.h"
+#include "bsp_usart.h"
+
+//外部变量
+extern DMA_HandleTypeDef hdma_usart1_tx;
+
+// /**
+//  * @brief kalman filter 类型
+//  *        0 为二阶
+//  *        1 为一阶 
+//  * 
+//  */
+// #define KALMAN_FILTER_TYPE 1
+
+//运算时间差
+#define DT 2
+//矩阵大小
+#define MATRIX_SIZE 4
+//状态矩阵大小
+#define STATUS_MATRIX_SIZE 2
+//H矩阵
+#define H_MATRIX_SIZE 2
+
+//pitch轴位移方差
+#define PITCH_DP 1
+//pitch轴速度方差
+#define PITCH_DV 1
+//pitch轴加速度方差
+#define PITCH_DA 1
+
+//yaw轴位移方差
+#define YAW_DP 1
+//yaw轴速度方差
+#define YAW_DV 1
+//yaw轴加速度方差
+#define YAW_DA 1
+
+//滤波后的角度偏移量
+#define GIMBAL_ANGLE_ADDRESS_OFFSET 0
+
+
+//一阶kalman filter参数
+#define GIMBAL_YAW_MOTOR_KALMAN_Q 400
+#define GIMBAL_YAW_MOTOR_KALMAN_R 400
+
+#define GIMBAL_PITCH_MOTOR_KALMAN_Q 200
+#define GIMBAL_PITCH_MOTOR_KALMAN_R 400
+
+
+//允许发弹角度误差
+#define ALLOW_ATTACK_ERROR 7
+
+//发弹判断计数次数
+#define JUDGE_ATTACK_COUNT 2
+//发弹停止判断计数次数
+#define JUDGE_STOP_ATTACK_COUNT 100
+
+//延时等待
+#define VISION_SEND_TASK_INIT_TIME 401
+//系统延时时间
+#define VISION_SEND_CONTROL_TIME_MS 10
+
+//延时等待
+#define VISION_TASK_INIT_TIME 450
+//系统延时时间
+#define VISION_CONTROL_TIME_MS 2
+
+//串口发送数据大小
+#define SERIAL_SEND_MESSAGE_SIZE 28
+//加倍
+#define DOUBLE_ 10000
+//上位机串口结构体
+#define VISION_USART huart1
+//上位机串口dma结构体
+#define VISION_TX_DMA hdma_usart1_tx
+//串口阻塞时间
+#define VISION_USART_TIME_OUT 1000
+//弧度制转角度制
+#define RADIAN_TO_ANGLE (360 / (2 * PI))
+
+#define HEAD1_DATA 0x34
+#define HEAD2_DATA 0X43
+
+//起始位1偏移量
+#define HEAD1_ADDRESS_OFFSET 0
+//起始位2偏移量
+#define HEAD2_ADDRESS_OFFSET 1
+//yaw轴偏移量
+#define YAW_ADDRESS_OFFSET 2
+//pitch轴偏移量
+#define PITCH_ADDRESS_OFFSET 3
+//roll轴偏移量
+#define ROLL_ADDRESS_OFFSET 4
+//模式转换偏移量
+#define SWITCH_ADDRESS_OFFSET 5
+//结尾数据偏移量
+#define END_ADDRESS_OFFSET 6
+
+//数据分离uint8_t的数组的大小
+#define UINT8_T_DATA_SIZE 4
+
+//向上位机发送数据为处理符号做加pi处理
+#define SEND_MESSAGE_ERROR PI
+
+//最大未接受到上位机数据的时间
+#define MAX_UNRX_TIME 400
+
+//上位机模式,包括装甲板模式,能量机关模式,前哨站模式
+typedef enum
+{
+    ARMOURED_PLATE = 1, //装甲板模式
+    ENERGY_ORGAN = 2,   //能量机关
+    OUTPOST = 3,        //前哨站
+}vision_mode_e;
+
+//32位数据转8位数据
+typedef union 
+{
+    float fp32_val;
+    uint32_t uint32_val;
+    uint8_t uin8_value[UINT8_T_DATA_SIZE];
+    
+}date32_to_date8_t;
+
+typedef struct 
+{
+    fp32 yaw;
+    fp32 pitch;
+    fp32 roll;
+}eular_angle_t;
+
+
+// //视觉kalman filter结构体
+// typedef struct 
+// {
+//     //一维kalman
+//     kalman gimbal_pitch_kalman;  //pitch轴电机kalman filter结构体    
+//     kalman gimbal_yaw_kalman;    //云台yaw轴电机kalman filter结构体
+//     kalman disdance_kalman;      //距离结构体    
+
+//     //二阶kalman filter,主要处理云台电机数据
+//     kalman_filter_t gimbal_pitch_second_order_kalman;
+//     kalman_filter_t gimbal_yaw_second_order_kalman;
+
+//     //二阶kalman filter 初始化结构体，用于给矩阵运算库的矩阵赋值
+//     kalman_filter_init_t gimbal_pitch_second_order_kalman_init;
+//     kalman_filter_init_t gimbal_yaw_second_order_kalman_init;
+// }vision_kalman_filter_t;
+
+//哨兵云台电机运动命令,经滤波处理后的数值
+typedef struct 
+{
+    //本次云台yaw轴数值
+    fp32 gimbal_yaw;
+    //本次云台pitch轴数值
+    fp32 gimbal_pitch;
+}gimbal_vision_control_t;
+
+//哨兵发射命令
+typedef enum
+{
+    SHOOT_ATTACK,       // 袭击
+    SHOOT_READY_ATTACK, // 准备袭击
+    SHOOT_STOP_ATTACK,  // 停止袭击
+} shoot_command_e;
+
+//哨兵发射电机运动控制命令
+typedef struct 
+{
+    //自动发射命令
+    shoot_command_e shoot_command;
+}shoot_vision_control_t;
+
+
+
+//视觉任务结构体
+typedef struct
+{
+    // 上位机视觉指针
+    vision_rxfifo_t *vision_rxfifo;
+    
+    // 自身绝对角指针
+    const fp32* vision_angle_point;
+    
+    // 绝对角
+    eular_angle_t absolution_angle;
+
+    // // kalman filter 结构体，用于处理视觉上位机发来的数据
+    // vision_kalman_filter_t vision_kalman_filter;
+
+    // 云台电机运动命令
+    gimbal_vision_control_t gimbal_vision_control;    
+
+    //发射机构发射命令
+    shoot_vision_control_t shoot_vision_control;
+
+} vision_control_t;
+
+//视觉发送任务结构体
+typedef struct
+{
+    
+    // 自身绝对角指针
+    const fp32 *vision_angle_point;
+    // 绝对角
+    eular_angle_t absolution_angle;
+    // 发送的绝对角数据，为消除负号对原始数据做加pi处理
+    eular_angle_t send_absolution_angle;
+    // 配置串口发送数据
+    uint8_t send_message[SERIAL_SEND_MESSAGE_SIZE];
+    // 配置串口名称(该为地址)
+    UART_HandleTypeDef *send_message_usart;
+    // 配置串口发送dma
+    DMA_HandleTypeDef *send_message_dma;
+
+}vision_send_t;
+
+
+//视觉通信任务
+void vision_send_task(void const *pvParameters);
+
+//视觉数据处理任务
+void vision_task(void const* pvParameters);
+
+// 获取上位机云台命令
+gimbal_vision_control_t* get_vision_gimbal_point(void);
+
+// 获取上位机发射命令
+shoot_vision_control_t* get_vision_shoot_point(void);
+
+/**
+ * @brief 判断未接收到上位机数据
+ * 
+ * @return 返回1 未接收到， 返回0 接收到 
+ */
+bool_t judge_not_rx_vision_data(void);
+
+#endif // !VISION_TASK_H
